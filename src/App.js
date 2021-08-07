efffd0129a91ff86b2bdcfe6c585a59545d81d42
satyadvaya <satyadvaya@gmail.com>
2021-08-06 18:27:30 -0700

    working ascending/descending dropdown along with URLSearchParams

--- src/App.js
+++ src/App.js
@@ -2,40 +2,58 @@ import React, { Component } from 'react';
 import PokeList from './PokeList.js';
 
 class App extends Component {
-  state = { data: [], loading: true, query: null };
-
-  updateQuery = (event) => {
-    this.setState({ query: event.target.value });
-  }
-
+  state = { data: [], loading: true, query: null, sortOrder: 'asc' };
+  
   componentDidMount() {
-    this.fetchData();
+    this.fetchPokemon();
   }
-
-  fetchData = async () => {
-    let url = "https://pokedex-alchemy.herokuapp.com/api/pokedex"
-    if (this.state.query) {
-      url = url + `?pokemon=${this.state.query}`;
+  
+  fetchPokemon = async () => {
+    if (!this.state.loading) {
+      this.setState({ loading: true });
     }
+
+    let url = "https://pokedex-alchemy.herokuapp.com/api/pokedex"
+    let searchParams = new URLSearchParams();
+
+    if (this.state.query) {
+      searchParams.set('pokemon', this.state.query);
+    }
+    if (this.state.sortOrder) {
+      searchParams.set('sort', 'pokemon');
+      searchParams.set('direction', this.state.sortOrder);
+    }
+
+    url = url + `?${searchParams.toString()}`;
     let response = await fetch(url);
-    console.log(response);
     let data = await response.json();
-    console.log(data);
+
     setTimeout(() => {
       this.setState({ data: data.results, loading: false });
     }, 1000);
   }
+  
+  updateQuery = (event) => {
+    this.setState({ query: event.target.value });
+  }
+  updateSort = (event) => {
+    this.setState({ sortOrder: event.target.value });
+  }
 
   render() {
-    const { loading } = this.state;
+    const { loading, sortOrder } = this.state;
     return ( 
       <>
         <h1>Pokemon Characters!</h1>
+        <select defaultValue={sortOrder} onChange={this.updateSort}>
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+        <input onChange={this.updateQuery} type='text'></input>
+        <button onClick={this.fetchPokemon}>Search!</button>
         {loading && <h3>Patience, Please ...</h3>}
         {!loading && (
           <section>
-            <input onChange={this.updateQuery} type='text'></input>
-            <button onClick={this.fetchData}>Search!</button>
             <PokeList pokedex={this.state.data} />
           </section>
         )}
