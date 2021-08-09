e4bff27d588e913d43240cd8b08f545ec4c2dd2d
satyadvaya <satyadvaya@gmail.com>
2021-08-09 16:24:54 -0700

    added Switch statement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import PokeList from './PokeList.js';
 import PokemonContainer from './PokemonContainer.js';
 
 class App extends Component {
 
   render() {
-    return <PokemonContainer />
+    return (
+      <section className='app'>
+        <BrowserRouter>
+          <Switch>
+              {/* <Route path='/pokemon/:pokeId' component={PokeDetail} /> */}
+              <Route path='/pokemon' component={PokemonContainer} />
+              <Route path='/' exact><h2>Welcome to pokedex</h2></Route>
+          </Switch>
+        </BrowserRouter>
+      </section>
+    )
   }
 }
  
