584f58f64c02b8d9538ca6c399f80144aa15bd51
satyadvaya <satyadvaya@gmail.com>
2021-08-09 18:52:12 -0700

    added PokeDetail.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import PokeList from './PokeList.js';
+// import PokeList from './PokeList.js';
 import PokemonContainer from './PokemonContainer.js';
 import Home from './Home.js';
 import Header from './Header.js';
@@ -14,7 +14,7 @@ class App extends Component {
         <BrowserRouter>
           <Header />
           <Switch>
-              <Route path='/pokemon/:pokeId' component={PokeDetail} />
+              <Route path='/pokemon/:id' component={PokeDetail} />
               <Route path='/pokemon' component={PokemonContainer} />
               {/* <Route path='/' exact><h2>Welcome to pokedex</h2></Route> */}
               <Route path='/' component={Home} />
--- src/PokeDetail.js
+++ src/PokeDetail.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
 class PokeDetail extends Component {
-    // state = {  }
+    state = {data: {} };
+    loadData = async () => {
+        const { id } = this.props.match.params;
+        const url = `https://pokedex-alchemy.herokuapp.com/api/pokedex/${id}`;
+        const resp = await fetch(url);
+        const data = await resp.json();
+        this.setState({ data });
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
     render() { 
         console.log(this.props.match);
+        const { id } = this.props.match.params;
+        const { data } = this.state;
         return (
             <section>
-                <h1>PokeDetail</h1>
+                <h1>{data.pokemon}</h1>
+                {/* <p>Poke ID: {id}</p> */}
+                <img src={data.url_image} alt='poke' />
             </section>
         );
     }
